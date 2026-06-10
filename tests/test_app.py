401f1c33695853818be45ea674cfdacc8c87b640
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2026-06-10 12:01:08 +0530

    feat: Added new changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,8 @@ import sys
 
 
 def test_app_prints_expected_message_and_directory():
-    project_dir = os.path.dirname(os.path.abspath(__file__))
+    test_dir = os.path.dirname(os.path.abspath(__file__))
+    project_dir = os.path.abspath(os.path.join(test_dir, ".."))
     result = subprocess.run(
         [sys.executable, os.path.join(project_dir, "app.py")],
         cwd=project_dir,
@@ -13,7 +14,26 @@ def test_app_prints_expected_message_and_directory():
         check=True,
     )
 
+# Alternatively to capture the output, you can use subprocess.check_output:
+
+# from pathlib import Path
+
+# # Path(__file__) is my-project/tests/test_pipeline.py
+# # .parent is my-project/tests/
+# # .parent.parent is my-project/
+# # / "src" makes it my-project/src/
+# project_dir = Path(__file__).parent.parent / "src"
+
+# result = subprocess.run(
+#     [sys.executable, str(project_dir / "app.py")],
+#     cwd=str(project_dir),
+#     capture_output=True,
+#     text=True,
+#     check=True,
+# )
+
+    print(f"--- DEBUG STDOUT ---\n{result.stdout}\n-------------------")
     assert "This is my second personal Dockerized Python App " in result.stdout
     assert "Current working directory:" in result.stdout
-    # assert "app.py" in result.stdout
+    assert "app.py" in result.stdout
     assert "Files in current directory:" in result.stdout
